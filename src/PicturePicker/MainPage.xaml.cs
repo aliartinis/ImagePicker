acf9550832530b751e787bf4a4085c66c53abd7e
ali_artinis <ali@artinis.com>
2021-11-25 14:18:21 +0100

    picture picker in code behind

--- src/PicturePicker/MainPage.xaml.cs
+++ src/PicturePicker/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,14 +19,35 @@ using Windows.UI.Xaml.Navigation;
 
 namespace PicturePicker
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			// Clear previous returned file name, if it exists, between iterations of this scenario
+			NameValueTextBlock.Text = "";
+			TypeValueTextBlock.Text = "";
+
+			FileOpenPicker openPicker = new FileOpenPicker();
+			openPicker.ViewMode = PickerViewMode.Thumbnail;
+			openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+			openPicker.FileTypeFilter.Add(".jpg");
+			openPicker.FileTypeFilter.Add(".jpeg");
+			openPicker.FileTypeFilter.Add(".png");
+			StorageFile file = await openPicker.PickSingleFileAsync();
+			
+			if (file != null)
+			{
+				NameValueTextBlock.Text = file.DisplayName;
+				TypeValueTextBlock.Text = file.FileType;
+			}
+		}
+	}
 }
