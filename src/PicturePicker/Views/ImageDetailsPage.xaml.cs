e7abcb274197bd5c5431379b54d383dc138287ec
ali_artinis <ali@artinis.com>
2021-11-30 21:47:43 +0100

    moving the main page content to separate pages
    Making the image selection button and image details section into two
    pages to have separation of concerns. These pages use for now the
    main page's view-model.

---
+++ src/PicturePicker/Views/ImageDetailsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ImagePicker.Core.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PicturePicker.Views
+{
+	/// <summary>
+	/// A page to display details of an image.
+	/// </summary>
+	public sealed partial class ImageDetailsPage : Page
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ImageDetailsPage"/> class.
+		/// </summary>
+		public ImageDetailsPage()
+		{
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// Gets or sets the view-model.
+		/// </summary>
+		public MainViewModel ViewModel
+		{
+			get; 
+			set;
+		}
+	}
+}
---
+++ src/PicturePicker/Views/ImageSelectionPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ImagePicker.Core.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace PicturePicker.Views
+{
+	/// <summary>
+	/// A page that provides a control for selecting an image.
+	/// </summary>
+	public sealed partial class ImageSelectionPage : Page
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ImageSelectionPage"/> class.
+		/// </summary>
+		public ImageSelectionPage()
+		{
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// Gets or sets the view-model.
+		/// </summary>
+		public MainViewModel ViewModel
+		{
+			get;
+			set;
+		}
+	}
+}
